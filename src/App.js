a79f2d3d52e82339a430822169a1f5471a261206
Likhith.Sathish <likithlik54@gmail.com>
2022-12-09 11:46:20 +0530

    Logic Building

--- src/App.js
+++ src/App.js
@@ -1,39 +1,86 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Form from "react-bootstrap/Form";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { MagnifyingGlass } from "react-loader-spinner";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  const [me, setme] = useState("");
-  const [san, setsan] = useState({id:0,name:'wait a minuite who are you '});
+  const [loader, setloader] = useState(false);
 
-  var user = [
-    { id: 1, name: "likith" },
-    { id: 2, name: "syed sohail" },
+  const [me, setme] = useState("");
+
+  const [rand, setRand] = useState("");
+
+  const [yname, setYname] = useState([
+    { id: 1, name: "syed sohail" },
+    { id: 2, name: "likith" },
     { id: 3, name: "dinesh" },
     { id: 4, name: "santhosh" },
     { id: 5, name: "sagar" },
-    { id: 5, name: "vp" },
-  ];
+    { id: 6, name: "Pradumna" },
+    { id: 7, name: "sushmitha" },
+    { id: 8, name: "veena" },
+    { id: 9, name: "vibha" },
+    { id: 10, name: "harshitha" },
+    { id: 11, name: "shashikala" },
+    { id: 12, name: "meghana" },
+    { id: 13, name: "Aishwarya" },
+    { id: 14, name: "veeru patil" },
+    { id: 15, name: "Manohar" },
+    { id: 16, name: "Sharath" },
+    { id: 17, name: "Alocius" },
+  ]);
 
   var santa = [
-    { id: 1, name: "A" },
-    { id: 2, name: "b" },
-    { id: 3, name: "c" },
-    { id: 4, name: "d" },
-    { id: 5, name: "e" },
+    { id: 1, name: "syed sohail" },
+    { id: 2, name: "likith" },
+    { id: 3, name: "dinesh" },
+    { id: 4, name: "santhosh" },
+    { id: 5, name: "sagar" },
+    { id: 6, name: "Pradumna" },
+    { id: 7, name: "sushmitha" },
+    { id: 8, name: "veena" },
+    { id: 9, name: "vibha" },
+    { id: 10, name: "harshitha" },
+    { id: 11, name: "shashikala" },
+    { id: 12, name: "meghana" },
+    { id: 13, name: "Aishwarya" },
+    { id: 14, name: "veeru patil" },
+    { id: 15, name: "Manohar" },
+    { id: 16, name: "Sharath" },
+    { id: 17, name: "Alocius" },
   ];
 
   function handleSelect(e) {
-    setme(e.target.value );
+    setme(e.target.value);
+    console.log(me)
   }
-  console.log(me);
-  
 
-  function handleClick(e) {
-    const random = user[Math.floor(Math.random() * user.length)];
-    console.log(random)
-    setsan(random);
+  function handleClick() {
+    setRand("");
+    if (me === "") {
+      alert("Choose your name");
+    } else {
+      setTimeout(() => {
+        const random = santa[Math.floor(Math.random() * santa.length)];
+        if (me === random.name) {
+          handleClick();
+        } else {
+          setRand(random);
+          setloader(false);
+        }
+      }, 2000);
+      setloader(true);
+    }
+  }
+
+  function handleDone(e) {
+    setYname(yname.filter((item) => item.name !== me));
+    console.log(rand.name);
+    setRand("");
+    setme("");
   }
 
   return (
@@ -48,7 +95,8 @@ function App() {
         <div>
           <label>Who are you?</label>
           <Form.Select className="users" onChange={handleSelect}>
-            {user.map((user) => (
+            <option hidden>Choose</option>
+            {yname.map((user) => (
               <option key={user.id}>{user.name}</option>
             ))}
           </Form.Select>
@@ -61,9 +109,30 @@ function App() {
         </div>
 
         <div id="username">
-          <h5 style={{width:'18vw'}}>{san.name}</h5>
+          {loader ? (
+            <MagnifyingGlass
+              visible={true}
+              height="80"
+              width="80"
+              ariaLabel="MagnifyingGlass-loading"
+              wrapperStyle={{}}
+              wrapperClass="MagnifyingGlass-wrapper"
+              glassColor="#c0efff"
+              color="#e15b64"
+            />
+          ) : (
+            ""
+          )}
+          <h5 style={{ width: "18vw" }}>Your santa is </h5>
+
+          <div id="wrapper">
+            <div id="container">
+              <h1>{rand.name}</h1>
+            </div>
+          </div>
         </div>
       </div>
+      <button onClick={handleDone}>Done</button>
     </div>
   );
 }
